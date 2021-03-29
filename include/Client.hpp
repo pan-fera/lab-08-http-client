883db678c78cc7f7cdf363deba989e1b714a3d5b
pan_fera <pan_fera@mail.ru>
2021-03-30 01:29:34 +0300

    add

---
+++ include/Client.hpp
@@ -0,0 +1,32 @@
+//
+// Copyright 2021 pan_fera
+//
+
+#ifndef INCLUDE_CLIENT_HPP_
+#define INCLUDE_CLIENT_HPP_
+#include <boost/beast/core.hpp>
+#include <boost/beast/http.hpp>
+#include <boost/beast/version.hpp>
+#include <boost/asio/connect.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <nlohmann/json.hpp>
+namespace beast = boost::beast;     // from <boost/beast.hpp>
+namespace http = beast::http;       // from <boost/beast/http.hpp>
+namespace net = boost::asio;        // from <boost/asio.hpp>
+using tcp = net::ip::tcp;           // from <boost/asio/ip/tcp.hpp>
+using json = nlohmann::json;
+
+class Client {
+ public:
+  Client(std::string username, std::string host, std::string port,
+         std::string target);
+  json send(std::string input);
+  friend std::ostream& operator<<(std::ostream &out, Client& client);
+ private:
+  std::string _username, _host, _port, _target;
+};
+
+#endif  // INCLUDE_CLIENT_HPP_
---
+++ sources/Client.cpp
@@ -0,0 +1,58 @@
+//
+// Copyright 2021 pan_fera
+//
+
+#include "Client.hpp"
+
+#include <utility>
+Client::Client(std::string username, std::string host, std::string port,
+std::string target):_username(std::move(username)), _host(std::move(host)),
+                                     _port(std::move(port)),
+                                     _target(std::move(target)){}
+json Client::send(std::string input){
+  int version = 11;
+
+  net::io_context ioc;
+
+  tcp::resolver resolver(ioc);
+  beast::tcp_stream stream(ioc);
+
+  auto const results = resolver.resolve(_host, _port);
+
+  stream.connect(results);
+
+  http::request<http::string_body> req{http::verb::post, _target,
+                                       version};
+  req.set(boost::beast::http::field::host, _host);
+  req.set(boost::beast::http::field::user_agent, _username);
+  req.body()="{\"input\":\""+input+"\"}";
+  req.prepare_payload();
+  http::write(stream, req);
+
+
+  beast::flat_buffer buffer;
+  http::response<http::string_body> res;
+
+  http::read(stream, buffer, res);
+  json response;
+
+  if (res.result() == http::status::ok)
+     response = json::parse(res.body());
+  else
+    response = res.body();
+
+  beast::error_code ec;
+  stream.socket().shutdown(tcp::socket::shutdown_both, ec);
+
+  if (ec && ec != beast::errc::not_connected)
+    throw beast::system_error{ec};
+  return response;
+}
+
+std::ostream& operator<<(std::ostream &out,  Client& client) {
+  std::string input;
+  std::cout << "Enter your request:" << std::endl;
+  std::cin >> input;
+  out << client.send(input).dump(4) << std::endl;
+  return out;
+}
